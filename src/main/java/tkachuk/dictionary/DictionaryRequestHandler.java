b3781814bf06148cb58626a892761708b7b5f1b6
Tkachuk, Adel <tkachukadel@gmail.com>
2025-12-03 18:09:16 -0500

    fixed up RequestHandler

--- src/main/java/tkachuk/dictionary/DictionaryRequestHandler.java
+++ src/main/java/tkachuk/dictionary/DictionaryRequestHandler.java
@@ -3,21 +3,60 @@ package tkachuk.dictionary;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 
-public class DictionaryRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, DictionaryResponse>
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class DictionaryRequestHandler implements
+        RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
 {
     private TouroDictionary touroDictionary;
+    private Gson gson;
+
+    public DictionaryRequestHandler()
+    {
+        this.touroDictionary = new TouroDictionary();
+        this.gson = new Gson();
+    }
 
     @Override
-    public DictionaryResponse handleRequest(APIGatewayProxyRequestEvent event, Context context)
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context)
     {
-        String body = event.getBody();
-        Gson gson = new Gson();
-        DictionaryRequest dictionaryRequest = gson.fromJson(body, DictionaryRequest.class);
-        String word = dictionaryRequest.getWord();
-        String definition = touroDictionary.lookUp(word);
+        try
+        {
+            String body = event.getBody();
+            DictionaryRequest dictionaryRequest = gson.fromJson(body, DictionaryRequest.class);
+            String definition = touroDictionary.lookUp(dictionaryRequest.getWord());
 
-        return new DictionaryResponse(word, definition);
+            DictionaryResponse dictionaryResponse = new DictionaryResponse(dictionaryRequest.getWord(), definition);
+
+            // create the HTTP response with the DictionaryResponse
+            String responseJson = gson.toJson(dictionaryResponse);
+            APIGatewayProxyResponseEvent apiResponse = new APIGatewayProxyResponseEvent();
+            apiResponse.setStatusCode(200);
+            apiResponse.setBody(responseJson);
+            return apiResponse;
+
+        } catch (Exception e)
+        {
+            // this prints the stack trace to the AWS log file
+            e.printStackTrace();
+
+            // this outputs the stack trace to the client
+            return toResponseEvent(e);
+        }
+    }
+
+    private APIGatewayProxyResponseEvent toResponseEvent(Exception e)
+    {
+        APIGatewayProxyResponseEvent apiResponse = new APIGatewayProxyResponseEvent();
+        apiResponse.setStatusCode(500);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        e.printStackTrace(printWriter);
+        apiResponse.setBody(stringWriter.toString());
+        return apiResponse;
     }
 }
